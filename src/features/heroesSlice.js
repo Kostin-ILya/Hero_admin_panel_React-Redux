17d9344ca924e6fb98709feae590b6dd34740d90
Ilya <lord-ilja@yandex.ru>
2023-08-05 16:23:50 +0300

    add _baseApi

--- src/features/heroesSlice.js
+++ src/features/heroesSlice.js
@@ -8,45 +8,34 @@ import { toast } from 'react-toastify'
 
 import useHTTP from '../hooks/useHTTP'
 
+const _baseApi = 'https://63e513d9c04baebbcdb33a8e.mockapi.io/heroes'
+
 const heroesAdapter = createEntityAdapter()
 const { request } = useHTTP()
 
 const fetchHeroes = createAsyncThunk('heroes/fetchHeroes', async () => {
-  return await request('https://63e513d9c04baebbcdb33a8e.mockapi.io/heroes')
+  return await request(_baseApi)
 })
 
 const deleteHero = createAsyncThunk(
   'heroes/deleteHero',
   async ({ id, name }) => {
     return toast
-      .promise(
-        request(
-          `https://63e513d9c04baebbcdb33a8e.mockapi.io/heroes/${id}`,
-          'DELETE'
-        ),
-        {
-          pending: 'Deleting a hero...',
-          success: `${name} deleted!`,
-          error: 'Deleting Error',
-        }
-      )
+      .promise(request(`${_baseApi}/${id}`, 'DELETE'), {
+        pending: 'Deleting a hero...',
+        success: `${name} deleted!`,
+        error: 'Deleting Error',
+      })
       .then((res) => res.id)
   }
 )
 
 const addHero = createAsyncThunk('heroes/addHero', async (newHero) => {
-  return toast.promise(
-    request(
-      'https://63e513d9c04baebbcdb33a8e.mockapi.io/heroes',
-      'POST',
-      newHero
-    ),
-    {
-      pending: 'Adding a hero...',
-      success: `${newHero.name} added!`,
-      error: 'Adding error',
-    }
-  )
+  return toast.promise(request(_baseApi, 'POST', newHero), {
+    pending: 'Adding a hero...',
+    success: `${newHero.name} added!`,
+    error: 'Adding error',
+  })
 })
 
 const initialState = heroesAdapter.getInitialState({
